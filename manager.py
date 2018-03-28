cc73261d9166c900f7f980f963a2f98137ab01f8
ovels <me@wzn.im>
2018-03-28 18:18:29 +0800

    uwsgi自动重启配置

--- manager.py
+++ manager.py
@@ -36,8 +36,8 @@ def get():
     if request.method == 'POST':
         return 'post'
     else :
-        return 'get'
+        return 'get111'
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=False,host='127.0.0.1',port=5000)
