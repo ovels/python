573c1bd91ba3ae6855ef5d86bd76e8d3646cdce8
ovels <me@wzn.im>
2018-03-28 17:56:03 +0800

    add static

--- manager.py
+++ manager.py
@@ -1,7 +1,7 @@
 from flask import Flask, url_for, request, render_template
 from jinja2 import Template
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static', static_url_path='/static')
 
 @app.route('/')
 @app.route('/<name>')
